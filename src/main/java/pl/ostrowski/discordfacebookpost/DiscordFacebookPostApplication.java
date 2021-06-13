909fdd03f8ed9fef8661c2342ad81b68773dd42f
Jedrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2021-06-13 13:27:38 +0200

    Initial commit

---
+++ src/main/java/pl/ostrowski/discordfacebookpost/DiscordFacebookPostApplication.java
@@ -0,0 +1,12 @@
+package pl.ostrowski.discordfacebookpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordFacebookPostApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscordFacebookPostApplication.class, args);
+  }
+}
---
+++ src/test/java/pl/ostrowski/discordfacebookpost/DiscordFacebookPostApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.ostrowski.discordfacebookpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordFacebookPostApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
