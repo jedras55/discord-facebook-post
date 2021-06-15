a0f3e33a1316da44ab8954954808d1dfbd937eb0
Jedrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2021-06-15 23:20:00 +0200

    Logging http request body

---
+++ src/main/java/pl/ostrowski/discordfacebookpost/LoggingFilterConfiguration.java
@@ -0,0 +1,19 @@
+package pl.ostrowski.discordfacebookpost;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingFilterConfiguration {
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setMaxPayloadLength(10000000);
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setIncludeHeaders(false);
+    return loggingFilter;
+  }
+}
