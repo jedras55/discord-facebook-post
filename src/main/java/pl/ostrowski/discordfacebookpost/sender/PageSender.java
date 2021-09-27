e3b9ad572977a550bc5cdc218e9f558d8dccd331
Jedrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2021-09-27 22:48:33 +0200

    Sending hook to Race Portal with ignoring SSL

--- src/main/java/pl/ostrowski/discordfacebookpost/sender/PageSender.java
+++ src/main/java/pl/ostrowski/discordfacebookpost/sender/PageSender.java
@@ -1,8 +1,21 @@
 package pl.ostrowski.discordfacebookpost.sender;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.Optional;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.ostrowski.discordfacebookpost.sender.dto.Hook;
@@ -12,7 +25,7 @@ import pl.ostrowski.discordfacebookpost.sender.dto.Hook;
 @Slf4j
 public class PageSender implements Sender {
 
-  private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate = restTemplate();
 
   @Value("${page_url}")
   private String pageUrl;
@@ -20,12 +33,52 @@ public class PageSender implements Sender {
   @Value("${page_token}")
   private String pageToken;
 
+  public RestTemplate restTemplate() {
+
+    RestTemplateBuilder builder = new RestTemplateBuilder();
+
+    TrustManager[] trustAllCerts =
+        new TrustManager[] {
+          new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+              return new X509Certificate[0];
+            }
+
+            public void checkClientTrusted(
+                java.security.cert.X509Certificate[] certs, String authType) {}
+
+            public void checkServerTrusted(
+                java.security.cert.X509Certificate[] certs, String authType) {}
+          }
+        };
+    SSLContext sslContext = null;
+    try {
+      sslContext = SSLContext.getInstance("SSL");
+      sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+    } catch (NoSuchAlgorithmException | KeyManagementException e) {
+      log.error(e.getMessage());
+    }
+
+    CloseableHttpClient httpClient =
+        HttpClients.custom()
+            .setSSLContext(sslContext)
+            .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+            .build();
+    HttpComponentsClientHttpRequestFactory customRequestFactory =
+        new HttpComponentsClientHttpRequestFactory();
+    customRequestFactory.setHttpClient(httpClient);
+    return builder.requestFactory(() -> customRequestFactory).build();
+  }
+
   @Override
   public void send(Hook hook) {
-    log.info("Sending " + hook);
+    log.info("Sending to page " + hook);
     String discordUrl = queryUrl();
     try {
-      restTemplate.postForEntity(discordUrl, hook, Object.class);
+      HttpStatus status = restTemplate.postForEntity(discordUrl, hook, HttpStatus.class).getBody();
+      if (Optional.ofNullable(status).map(HttpStatus::is4xxClientError).orElse(true)) {
+        log.error("Forbidden");
+      }
     } catch (Exception e) {
       log.error(e.getMessage());
     }
@@ -34,6 +87,6 @@ public class PageSender implements Sender {
   private String queryUrl() {
     if (pageUrl == null || pageToken == null)
       throw new IllegalStateException("Page parameters not set");
-    return pageUrl + "/api/webhooks/facebook" + pageToken;
+    return pageUrl + "/api/webhooks/facebook/" + pageToken;
   }
 }
