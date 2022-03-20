e52432880d12b149ec96b632b54ae58bffcfde2a
Jedrzej Ostrowski <jedrzej.ostrowski@gmail.com>
2022-03-20 02:33:20 +0100

    Fixed too many requests

--- src/main/java/pl/ostrowski/discordfacebookpost/sender/DiscordSender.java
+++ src/main/java/pl/ostrowski/discordfacebookpost/sender/DiscordSender.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import pl.ostrowski.discordfacebookpost.sender.dto.Hook;
 
@@ -24,13 +25,28 @@ public class DiscordSender implements Sender {
   public void send(Hook hook) {
     log.info("Sending " + hook);
     String discordUrl = queryUrl();
-    hook.clipHooks()
-        .forEach(clippedHook -> restTemplate.postForEntity(discordUrl, clippedHook, Object.class));
+    hook.clipHooks().forEach(clippedHook -> send(clippedHook, discordUrl, 0));
   }
 
   private String queryUrl() {
     if (discordWebhookId == null || discordWebhookToken == null)
       throw new IllegalStateException("Discord parameters not set");
     return "https://discord.com/api/webhooks/" + discordWebhookId + "/" + discordWebhookToken;
+  }
+
+  private void send(Hook hook, String discordUrl, int tryCounter) {
+    try {
+      restTemplate.postForEntity(discordUrl, hook, Object.class);
+    } catch (RestClientException e) {
+      log.error(e.getMessage());
+      if (tryCounter <= 3) {
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+          log.error(e.getMessage());
+        }
+        send(hook, discordUrl, tryCounter + 1);
+      }
+    }
   }
 }
